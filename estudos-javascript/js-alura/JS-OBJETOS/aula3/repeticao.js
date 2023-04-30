c8af584d301119a57b3a5c875a8885151367ec0f
Clara Martins <95709670+clara-silva-martins@users.noreply.github.com>
2023-04-30 14:59:00 -0300

    for in em objetos

---
+++ estudos-javascript/js-alura/JS-OBJETOS/aula3/repeticao.js
@@ -0,0 +1,23 @@
+const cliente = {
+    nome: "Joao",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ["0123456789","7894561230"],
+};
+
+cliente.enderecos = [ {
+    rua: "R. São João",
+    numero: 10,
+    apartamento: true,
+    complemento: "ap 934",
+},
+];
+
+for (let chave in cliente) {
+    let tipo =  typeof cliente[chave];
+    if ( tipo !== 'object' && tipo != 'function' ){
+    console.log(`A chave ${chave} tem o valor ${cliente[chave]} `);
+    } else {
+        
+    }
+}
