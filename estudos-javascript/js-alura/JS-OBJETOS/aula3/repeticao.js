35c20a70ee5868d050ec77e5e4e5c2201f253fc5
Clara Martins <95709670+clara-silva-martins@users.noreply.github.com>
2023-05-02 19:25:52 -0300

    sem else

--- estudos-javascript/js-alura/JS-OBJETOS/aula3/repeticao.js
+++ estudos-javascript/js-alura/JS-OBJETOS/aula3/repeticao.js
@@ -17,7 +17,5 @@ for (let chave in cliente) {
     let tipo =  typeof cliente[chave];
     if ( tipo !== 'object' && tipo != 'function' ){
     console.log(`A chave ${chave} tem o valor ${cliente[chave]} `);
-    } else {
-        
-    }
+    } 
 }
