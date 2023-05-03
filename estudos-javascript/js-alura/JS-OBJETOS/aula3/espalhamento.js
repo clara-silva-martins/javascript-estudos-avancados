69311dd0278b5a1b275fd0339984a6c9f18499be
Clara Martins <95709670+clara-silva-martins@users.noreply.github.com>
2023-05-02 21:27:58 -0300

    spread operator

--- estudos-javascript/js-alura/JS-OBJETOS/aula3/espalhamento.js
+++ estudos-javascript/js-alura/JS-OBJETOS/aula3/espalhamento.js
@@ -12,3 +12,33 @@ cliente.enderecos = [ {
     complemento: "ap 934",
 },
 ];
+
+function ligaParaCliente(telefoneComercial, telefoneResidencial){
+    console.log(`Ligando para ${telefoneComercial}`);
+    console.log(`Ligando para ${telefoneResidencial}`);
+}
+
+// ligaParaCliente(cliente.telefone[0], cliente.telefone[1]);
+
+ligaParaCliente(...cliente.telefone);
+
+
+/*
+É uma função externa, então não tem como alterar ela, a função tem dois parametros chamando o telefone comercial e outra chamando o telefone residencial. Por fim chama a funçao na posição de indice 0 e de indice 1, que são os dois numeros cadastrados.
+
+Com o espalhamanto, ao invez de usar dessa forma escrevendo o indice, pode usar o metodo de espalhamento, que vai chamar a função do mesmo jeito. Essa sintaxe de espalhamento fez com que espalhasse o array no momento da chamada
+
+Com arrays e funções é possivel fazer
+
+Mas e com objetos? Também é possível usar a sintaxe de espalhamento com objetos tbm
+
+*/
+
+const encomenda = {
+    destinatario: cliente.nome,
+    ...cliente.enderecos[0],
+};
+
+console.log(encomenda);
+
+//Com essa sintaxe de espalhamento fica apenas um objeto, não deixa fragmentado, um objeto, depois uma lista com um objeto, todos compõe o mesmo, ou seja ele pega todo o conteudo do objeto e espalha todos os seus valores na chamada do console.log
